b12181d6a59e47c77ee3e665c0a16a94b52b25ec
Aryaman Verma <aryamanskate01@gmail.com>
2024-02-19 19:26:34 +0530

    Singup With Otp Added in Frontend

--- frontend/src/services/operations/authAPI.js
+++ frontend/src/services/operations/authAPI.js
@@ -39,3 +39,68 @@ const {
       toast.dismiss(toastId)
     }
   }
+
+  export function sendOtp(email, navigate) {
+    return async (dispatch) => {
+      const toastId = toast.loading("Loading...")
+      dispatch(setLoading(true))
+      try {
+        const response = await apiConnector("POST", "http://localhost:5000/api/auth/sendotp", {
+          email,
+          checkUserPresent: true,
+        })
+        console.log("SENDOTP API RESPONSE............", response)
+  
+        console.log(response.data.success)
+  
+        if (!response.data.success) {
+          throw new Error(response.data.message)
+        }
+  
+        toast.success("OTP Sent Successfully")
+        navigate("/verify-email")
+      } catch (error) {
+        console.log("SENDOTP API ERROR............", error)
+        toast.error("Could Not Send OTP")
+      }
+      dispatch(setLoading(false))
+      toast.dismiss(toastId)
+    }
+  }
+  
+  export function signUp(
+    username,
+    email,
+    password,
+    confirmPassword,
+    otp,
+    navigate
+  ) {
+    return async (dispatch) => {
+      const toastId = toast.loading("Loading...")
+      dispatch(setLoading(true))
+      try {
+        const response = await apiConnector("POST", "http://localhost:5000/api/auth/signup", {
+          username,
+          email,
+          password,
+          confirmPassword,
+          otp,
+        })
+  
+        console.log("SIGNUP API RESPONSE............", response)
+  
+        if (!response.data.success) {
+          throw new Error(response.data.message)
+        }
+        toast.success("Signup Successful")
+        navigate("/community")
+      } catch (error) {
+        console.log("SIGNUP API ERROR............", error)
+        toast.error("Signup Failed")
+        navigate("/signup")
+      }
+      dispatch(setLoading(false))
+      toast.dismiss(toastId)
+    }
+  }
