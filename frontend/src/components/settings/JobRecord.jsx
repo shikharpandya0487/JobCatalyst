2db190c3c38c48e89954c0dd06c9876ec077bb57
Aradhya Singh <aradhyasingh04937@gmail.com>
2024-04-07 00:17:14 +0530

    added jobpage and integrate jobRecord page

--- frontend/src/components/settings/JobRecord.jsx
+++ frontend/src/components/settings/JobRecord.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
-import { useTheme } from '../../Context/ThemeContext'; 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useTheme } from "../../Context/ThemeContext";
 const JobRecord = () => {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   const [jobRecords, setJobRecords] = useState([]);
   const [savedRecords, setSavedRecords] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetchJobRecords();
+  }, []);
+
+  const fetchJobRecords = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        "/api/profile/jobrecords/getAllJobRecord"
+      );
+      setSavedRecords(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+      setIsLoading(false);
+    }
+  };
   const addJobRecord = () => {
     setJobRecords([
       ...jobRecords,
@@ -13,7 +33,6 @@ const JobRecord = () => {
         position: "",
         startDate: "",
         endDate: "",
-        description: "",
       },
     ]);
   };
@@ -25,10 +44,38 @@ const JobRecord = () => {
     setJobRecords(updatedRecords);
   };
 
-  const saveRecord = (index) => {
+  // const saveRecord = (index) => {
+  //   const recordToSave = jobRecords[index];
+  //   setSavedRecords([...savedRecords, recordToSave]);
+  //   setJobRecords(jobRecords.filter((_, i) => i !== index));
+  // };
+  const saveRecord = async (index) => {
     const recordToSave = jobRecords[index];
-    setSavedRecords([...savedRecords, recordToSave]);
-    setJobRecords(jobRecords.filter((_, i) => i !== index));
+    try {
+      setIsLoading(true);
+      const response = await axios.post(
+        "/api/profile/jobrecords/createJobRecord",
+        recordToSave
+      );
+      setSavedRecords([...savedRecords, response.data.data]);
+      setJobRecords(jobRecords.filter((_, i) => i !== index));
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+      setIsLoading(false);
+    }
+  };
+
+  const deleteRecord = async (postId) => {
+    try {
+      setIsLoading(true);
+      await axios.post("/api/profile/jobrecords/deleteJobRecord", { postId });
+      setSavedRecords(savedRecords.filter((record) => record._id !== postId));
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -49,13 +96,17 @@ const JobRecord = () => {
       >
         Add Record
       </button>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
       {jobRecords.map((record, index) => (
-        <div key={index} className="bg-gray-100 p-4 rounded shadow mb-4"
-        style={{
-          backgroundColor: theme === "dark" ? "#333" : "#fff",
-          color: theme === "dark" ? "#fff" : "#333",
-          border:theme== "dark" ? ' 1px solid #fff': '',
-        }}
+        <div
+          key={index}
+          className="bg-gray-100 p-4 rounded shadow mb-4"
+          style={{
+            backgroundColor: theme === "dark" ? "#333" : "#fff",
+            color: theme === "dark" ? "#fff" : "#333",
+            border: theme == "dark" ? " 1px solid #fff" : "",
+          }}
         >
           <h5 className="mb-2 font-semibold pb-4 italic">
             Work Experience {index + 1}
@@ -69,7 +120,7 @@ const JobRecord = () => {
               name="company"
               value={record.company}
               onChange={(e) => handleChange(index, e)}
-              className="input-field bg-white rounded-md p-1 w-3/4 hover:cursor-pointer"
+              className="input-field bg-white text-black rounded-md p-1 w-3/4 hover:cursor-pointer"
             />
           </div>
           <div className=" w-full p-1">
@@ -81,7 +132,7 @@ const JobRecord = () => {
               name="position"
               value={record.position}
               onChange={(e) => handleChange(index, e)}
-              className="input-field bg-white rounded-md p-1 w-3/4 hover:cursor-pointer"
+              className="input-field bg-white text-black rounded-md p-1 w-3/4 hover:cursor-pointer"
             />
           </div>
           <div className=" w-full p-1">
@@ -93,7 +144,7 @@ const JobRecord = () => {
               name="startDate"
               value={record.startDate}
               onChange={(e) => handleChange(index, e)}
-              className="input-field bg-white rounded-md p-1 w-3/4 hover:cursor-pointer"
+              className="input-field bg-white text-black rounded-md p-1 w-3/4 hover:cursor-pointer"
             />
           </div>
           <div className=" w-full p-1">
@@ -105,10 +156,10 @@ const JobRecord = () => {
               name="endDate"
               value={record.endDate}
               onChange={(e) => handleChange(index, e)}
-              className="input-field bg-white rounded-md p-1 w-3/4 hover:cursor-pointer"
+              className="input-field bg-white text-black rounded-md p-1 w-3/4 hover:cursor-pointer"
             />
           </div>
-          <div className=" w-full p-1">
+          {/* <div className=" w-full p-1">
             <label className="font-light font-serif w-1/4 text-xl text-center text-black">
               Description:
             </label>
@@ -116,9 +167,9 @@ const JobRecord = () => {
               name="description"
               value={record.description}
               onChange={(e) => handleChange(index, e)}
-              className="input-field bg-white rounded-md p-1 w-3/4 hover:cursor-pointer"
+              className="input-field bg-white  text-black rounded-md p-1 w-3/4 hover:cursor-pointer"
             />
-          </div>
+          </div> */}
           <button
             onClick={() => saveRecord(index)}
             className="bg-green-500 text-white py-2 px-4 mt-2 rounded"
@@ -128,7 +179,13 @@ const JobRecord = () => {
         </div>
       ))}
       {savedRecords.map((record, index) => (
-        <div key={index} className="bg-gray-100 p-4 rounded shadow mb-4">
+        <div key={index} className=" p-4 rounded shadow mb-4"
+        style={{
+          backgroundColor: theme === "dark" ? "#333" : "#fff",
+          color: theme === "dark" ? "#fff" : "#333",
+          border: theme == "dark" ? " 1px solid #fff" : "",
+        }}
+          >
           <h5 className="mb-2 font-semibold pb-4 italic">
             Work Experience {index + 1}
           </h5>
@@ -144,9 +201,15 @@ const JobRecord = () => {
           <p>
             <strong>End Date:</strong> {record.endDate}
           </p>
-          <p>
+          {/* <p>
             <strong>Description:</strong> {record.description}
-          </p>
+          </p> */}
+          <button
+            onClick={() => deleteRecord(record._id)}
+            className="bg-red-500 text-white py-2 px-4 mt-2 rounded"
+          >
+            Delete Record
+          </button>
         </div>
       ))}
     </div>
